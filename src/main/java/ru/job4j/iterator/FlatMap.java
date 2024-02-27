8d4b40e771185c1ca49247c87a220f54144778cc
Towaris4 <ivan.tarasov2488@gmail.com>
2024-02-27 23:48:57 +0300

    4. FlatMap для Iterator<Iterator> [#152 #483397]

--- src/main/java/ru/job4j/iterator/FlatMap.java
+++ src/main/java/ru/job4j/iterator/FlatMap.java
@@ -15,12 +15,8 @@ public class FlatMap<T> implements Iterator<T> {
 
     @Override
     public boolean hasNext() {
-        while (!cursor.hasNext()) {
-            if (data.hasNext()) {
-                cursor = data.next();
-            } else {
-                break;
-            }
+        while ((!cursor.hasNext()) && (data.hasNext())) {
+            cursor = data.next();
         }
         return cursor.hasNext();
     }
