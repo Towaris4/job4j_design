7963fed06cc5900e040007b6d2f0522a9f5a90aa
Towaris4 <ivan.tarasov2488@gmail.com>
2024-03-14 00:21:49 +0300

    10. Реализовать собственную структуру данных - HashMap [#1008]

--- src/main/java/ru/job4j/map/NonCollisionMap.java
+++ src/main/java/ru/job4j/map/NonCollisionMap.java
@@ -1,7 +1,9 @@
 package ru.job4j.map;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class NonCollisionMap<K, V> implements SimpleMap<K, V> {
 
@@ -18,12 +20,17 @@ public class NonCollisionMap<K, V> implements SimpleMap<K, V> {
 
     @Override
     public boolean put(K key, V value) {
+        if (((float) count / capacity) >= LOAD_FACTOR) {
+            expand();
+        }
         boolean result = false;
-        int index = indexFor(hash(key.hashCode()));
+        int index = indexFor(hash(Objects.hashCode(key)));
         if (table[index] == null) {
             result = true;
             table[index] = new MapEntry<K, V>(key, value);
         }
+        count++;
+        modCount++;
         return result;
     }
 
@@ -36,17 +43,50 @@ public class NonCollisionMap<K, V> implements SimpleMap<K, V> {
     }
 
     private void expand() {
-
+        capacity = capacity * 2;
+        MapEntry<K, V>[] newTable = new MapEntry[capacity];
+        Iterator<K> iterator = iterator();
+        for (MapEntry<K, V> map : table) {
+            if (map != null) {
+                K key = map.key;
+                int hashcode = Objects.hashCode(key);
+                int hash = hash(hashcode);
+                int index = hash & (newTable.length - 1);
+                newTable[index] = map;
+            }
+        }
+        table = newTable;
     }
 
     @Override
     public V get(K key) {
-        return null;
+            V result = null;
+            int hashcode = Objects.hashCode(key);
+            int hash = hash(hashcode);
+            int index = indexFor(hash(hash));
+            if (table[index] != null
+                    && Objects.hashCode(table[index].key) == hash
+                    && Objects.equals(table[index].key, key)) {
+                result = table[index].value;
+            }
+            return result;
     }
 
     @Override
     public boolean remove(K key) {
-        return false;
+        boolean result = false;
+        int hashcode = Objects.hashCode(key);
+        int hash = hash(hashcode);
+        int index = indexFor(hash(hash));
+        if (table[index] != null
+                && Objects.hashCode(table[index].key) == hash
+                && Objects.equals(table[index].key, key)) {
+            table[index] = null;
+            result = true;
+        }
+        count--;
+        modCount--;
+        return result;
     }
 
     @Override
@@ -55,9 +95,13 @@ public class NonCollisionMap<K, V> implements SimpleMap<K, V> {
         return new Iterator<K>() {
 
             private int index = 0;
+            private int expectedModCount = modCount;
 
             @Override
             public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
                 while ((index < table.length) && (table[index] == null)) {
                     index++;
                 }
--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -11,5 +14,36 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Calendar birthday = Calendar.getInstance();
+        Map<User, Object> map = new HashMap<>(16);
+        User userOne = new User("Ivan", 0, birthday);
+        int hashcode1 = userOne.hashCode();
+        int hash1 = hashcode1 ^ (hashcode1 >>> 16);
+        int bucked1 = hash1 & 15;
+        User userTwo = new User("Ivan", 0, birthday);
+        int hashcode2 = userTwo.hashCode();
+        int hash2 = hashcode2 ^ (hashcode2 >>> 16);
+        int bucked2 = hash2 & 15;
+        map.put(userOne, new Object());
+        map.put(userTwo, new Object());
+        for (User user : map.keySet()) {
+            System.out.println(map.get(user).toString());
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        User user = (User) obj;
+        return (this.birthday == user.birthday)
+                && (this.name == user.name)
+                && (this.children == user.children);
+    }
+
+   @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 }
