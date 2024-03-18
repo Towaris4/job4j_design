6970ffb014413e2e11ae69633506b97a1760e964
Towaris4 <ivan.tarasov2488@gmail.com>
2024-03-18 19:47:34 +0300

    3. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,12 @@
+package ru.job4j.question;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Analize {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        Set<User> previous
+    }
+}
