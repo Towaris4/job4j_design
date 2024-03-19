d45ba366e207c6ed7689664008863ecb06d797b1
Towaris4 <ivan.tarasov2488@gmail.com>
2024-03-19 12:44:13 +0300

    3. Статистика по коллекции. [#45889]

--- src/main/java/ru/job4j/question/User.java
+++ src/main/java/ru/job4j/question/User.java
@@ -30,17 +30,19 @@ public class User {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         User user = (User) o;
-        return id == user.id && Objects.equals(name, user.name);
+        return id == user.id
+                && Objects.equals(name, user.name);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(id, name);
     }
-
-    /* Геттеры, сеттеры, equals() & hashCode() */
-
 }
