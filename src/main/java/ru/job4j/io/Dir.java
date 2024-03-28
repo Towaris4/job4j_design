d5d70b275b3d9072f6dd0f1c34629a0b1a1f2626
Towaris4 <ivan.tarasov2488@gmail.com>
2024-03-28 17:29:02 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("c:\\projects");
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
@@ -13,7 +13,7 @@ public class Dir {
         }
         for (File subfile : file.listFiles()) {
             if (subfile.isFile()) {
-                System.out.printf("Имя файла: %s Размер файла: %s", subfile.getName(), subfile.length());
+                System.out.printf("Имя файла: %s Размер файла: %s\n", subfile.getName(), subfile.length());
             }
         }
     }
--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,8 +10,9 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("");
-        for (Path path : search(start, path -> path.toFile().getName().endsWith(".class"))) {
+        validateArgs(args);
+        Path start = Paths.get(args[0]);
+        for (Path path : search(start, path -> path.toFile().getName().endsWith(args[1]))) {
             System.out.println(path.toFile().getName());
         }
     }
@@ -20,4 +22,13 @@ public class Search {
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
+     private static void validateArgs(String[] args) {
+        File file = new File(args[0]);
+         if (!file.isDirectory()) {
+             throw new IllegalArgumentException(String.format("Not directory %s", args[0]));
+         }
+         if (!args[1].startsWith(".")) {
+             throw new IllegalArgumentException(String.format("Not expansion %s", args[1]));
+         }
+     }
 }
