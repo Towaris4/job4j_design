fe181c45f54f519a810cc0c05f1bb4cb3d36f8ad
Towaris4 <ivan.tarasov2488@gmail.com>
2024-04-01 20:44:03 +0300

    16. Кодировка [#862]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -1,10 +1,11 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.Random;
 
 public class ConsoleChat {
     private static final String OUT = "закончить";
@@ -19,7 +20,33 @@ public class ConsoleChat {
     }
 
     public void run() {
-
+        List<String> log = new ArrayList<>();
+        List<String> answerList = readPhrases();
+        boolean run = true;
+        boolean bot = true;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            String command = scanner.nextLine();
+            log.add(command);
+            switch (command) {
+                case OUT:
+                    run = false;
+                    break;
+                case STOP:
+                    bot = false;
+                    break;
+                case CONTINUE:
+                    bot = true;
+                    break;
+            }
+            if (run && bot) {
+                Random random = new Random();
+                String answer = answerList.get(random.nextInt(answerList.size()));
+                log.add(answer);
+                System.out.println(answer);
+            }
+        } while (run);
+        saveLog(log);
     }
 
     private List<String> readPhrases() {
@@ -33,11 +60,15 @@ public class ConsoleChat {
     }
 
     private void saveLog(List<String> log) {
-
+        try (PrintWriter writer = new PrintWriter(new FileWriter(path, StandardCharsets.UTF_8, true))) {
+            log.forEach(writer::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        ConsoleChat consoleChat = new ConsoleChat(".", "data/botAnswers.txt");
+        ConsoleChat consoleChat = new ConsoleChat("data/consoleChat.txt", "data/botAnswers.txt");
         consoleChat.run();
     }
 }
