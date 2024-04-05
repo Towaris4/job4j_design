3d30d7632399099917225eecba873a8579cd5fda
Towaris4 <ivan.tarasov2488@gmail.com>
2024-04-05 11:52:26 +0300

    17. Scanner [#504791 #489617]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -10,25 +10,26 @@ import java.util.zip.ZipOutputStream;
 public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
         File file = new File(argsName.get("path"));
+        String delimiter = argsName.get("delimiter");
         try (var scanner = new Scanner(file)) {
             String firstLine = scanner.nextLine();
-            List<String> nameColumns = Arrays.asList(firstLine.split(argsName.get("delimiter")));
+            List<String> nameColumns = Arrays.asList(firstLine.split(delimiter));
             String[] filter = argsName.get("filter").split(",");
             StringBuilder stringBuilder = new StringBuilder();
             int[] index = new int[filter.length];
             for (int i = 0; i < filter.length; i++) {
                 index[i] = nameColumns.indexOf(filter[i]);
             }
-            stringBuilder.append(String.join(argsName.get("delimiter"), filter)).append(System.lineSeparator());
+            stringBuilder.append(String.join(delimiter, filter)).append(System.lineSeparator());
             while (scanner.hasNext()) {
-                String[] line = scanner.nextLine().split(argsName.get("delimiter"));
+                String[] line = scanner.nextLine().split(delimiter);
                 for (int i = 0; i < filter.length - 1; i++) {
                     stringBuilder.append(line[index[i]]);
-                    stringBuilder.append(argsName.get("delimiter"));
+                    stringBuilder.append(delimiter);
                 }
                 stringBuilder.append(line[index[filter.length - 1]]).append(System.lineSeparator());
             }
-            if (argsName.get("out").equals("stdout")) {
+            if ("stdout".equals(argsName.get("out"))) {
                 System.out.print(stringBuilder.toString());
             } else {
                 try (PrintWriter writer = new PrintWriter(new FileWriter(argsName.get("out"), StandardCharsets.UTF_8, true))) {
@@ -50,7 +51,7 @@ public class CSVReader {
         if (!argsName.get("path").endsWith(".csv")) {
                 throw new IllegalArgumentException("The file name does not have a \".csv\" extension");
         }
-        if (!argsName.get("out").equals("stdout") || (new File(argsName.get("out"))).isFile()) {
+        if (!"stdout".equals(argsName.get("out")) || (new File(argsName.get("out"))).isFile()) {
             throw new IllegalArgumentException("stdout or path");
         }
         handle(argsName);
