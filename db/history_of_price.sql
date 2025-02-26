c58c6c346d23440115fd6916385e526ffd316092
Towaris4 <ivan.tarasov2488@gmail.com>
2025-02-26 20:05:56 +0300

    2. Триггеры [#504803 #532639]

--- db/history_of_price.sql
+++ db/history_of_price.sql
@@ -35,7 +35,8 @@ or replace function tax_trigger_row()
 $$
     BEGIN
         update products
-        set price = price + price * 0.2;
+        set price = price + price * 0.2
+        where id = (select id from inserted);
         return NEW;
     END;
 $$
